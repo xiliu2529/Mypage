8e97638ae2c5038da0ed2e592c5c5d8235f38937
xiliu <130113225+xiliu2529@users.noreply.github.com>
2025-07-18 00:42:33 +0900

    修改base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ 这个非常重要！必须是相对路径
   plugins: [react()],
 })
